7e759268cfe3c148af1e88af58774b295f635053
Benoit Parmentier <benoit.parmentier@gmail.com>
2013-03-30 10:45:00 -0400

    S and T modes, comparison to IDRISI output

--- pca_rotation_test.R
+++ pca_rotation_test.R
@@ -1,3 +1,93 @@
+############################      SCRIPT 2 MEOT-PCA          #######################################
+#This script carries out a PCA with varimax rotation for a timeseries of 312 SST images.       #
+#Note that spatial patterns from MEOT and MSSA components are not analyzed in this script       #                 
+#AUTHOR: Benoit Parmentier                                                                      #
+#DATE: 03/28/2013            
+#Version: 4
+#PROJECT: Clark Labs Climate predction- MEOT/MSSA paper                                         #
+#################################################################################################
+
+###Loading R library and packages                                                      
+library(gtools)                              # loading some useful tools 
+library(mgcv)                                # GAM package by Simon Wood
+library(sp)                                  # Spatial pacakge with class definition by Bivand et al.
+library(spdep)                               # Spatial pacakge with methods and spatial stat. by Bivand et al.
+library(rgdal)                               # GDAL wrapper for R, spatial utilities
+library(gstat)                               # Kriging and co-kriging by Pebesma et al.
+library(fields)                              # NCAR Spatial Interpolation methods such as kriging, splines
+library(raster)                              # Hijmans et al. package for raster processing
+library(foreign)                             # Library for format exchange (e.g. dbf,spss,sas etc.)
+library(gdata)                               # various tools with xls reading
+library(xts)                                 # basic package for time series analysis
+library(zoo)                                 # basic package for time series analysis
+#library(forecast)                            # package containing ARIMA procedures
+library(rasterVis)
+library(psych)
+library(GPArotation)
+
+## Functions used in the script
+
+### TO DO ... format the data for MSSA create a function for the window of any size ...use lag???
+#unction ot lage files
+
+load_obj <- function(f) 
+{
+  env <- new.env()
+  nm <- load(f, env)[1]  
+  env[[nm]]
+}
+
+pca_to_raster_fun<-function(pc_spdf,ref_raster,NA_val,out_prefix){
+  #Input arguments:
+  #pc_spdf: spdf with scores components and
+  #         must include x,y in the last 2 columns
+  #ref_raster: reference raster giving the output extent and NA if present
+  #NA_val<- values assigned to NA pixels, if "NA", use default
+  
+  npc<-ncol(pc_spdf)-2 #number of components 
+  pc_scores_lf<-vector("list",npc) 
+  for (k in 1:npc){
+    pc_scores<-pc_spdf[,k] #vector containing scores for components k
+    pc_name<-names(pc_spdf)[k] #name of the current component
+    raster_name<-paste("pc_component_",k,"_",out_prefix,".rst",sep="")
+    
+    if (NA_val=="NA"){
+      pc_lag<-rasterize(pc_scores,ref_raster,pc_name,fun=min,overwrite=TRUE,
+                        filename=raster_name)
+    }
+    #Use provided NA value for the background
+    if (NA_val!="NA"){
+      pc_lag<-rasterize(pc_scores,ref_raster,pc_name,background=NA_val,fun=min,overwrite=TRUE,
+                        filename=raster_name)
+    }
+    pc_scores_lf[[k]]<-raster_name
+  } 
+  return(pc_scores_lf)
+}
+
+### Parameters and argument
+
+infile1<-"SAODI-01-1854_06-2011_test.asc"             #GHCN shapefile containing variables for modeling 2010                 
+infile2<-"SAODI-01-1854_06-2011.csv"                     #List of 10 dates for the regression
+infile3<-"MEOT_MSSA_Telcon_indices_12112012.xlsx"                        #LST dates name
+infile4<-"mask_rgf_1_1.rst"
+infile_pca<-"pca_IDRISI_03302013_T-Mode_DIF.xlsx"
+
+npc<-20 # number of pca to produce...put this at the beginning of the script...
+lag_window<-13
+telind<-c("PNA","NAO","TNA","TSA","SAOD","MEI","PDO","AO","AAO","AMM","AMOsm","QBO")
+mode_list_MEOT<-c("MEOT1","MEOT3", "MEOT4","MEOT7","MEOT10","MEOT15","MEOT16")
+mode_list_PCA<-c("MSSA1","MSSA2","MSSA3","MSSA4","MSSA5","MSSA6")    
+#mode_list_PCA<-paste("MSSA",1:15,sep="")
+out_prefix<-"pca_test_03302013"
+
+# on Benoit Mac
+in_path<-"/Users/benoitparmentier/Dropbox/Data/MEOT_paper/MEOT12272012/MEOT_working_dir_03102013"
+# on Atlas:
+#in_path<-"/home/parmentier/Data/MEOT12272012/MEOT_working_dir_03102013/"
+
+setwd(in_path)
+
 ########################################
 
 ### STEP 0: READ IN DATASETS RELATED TO TELECONNECTION AND PREVIOUS LOADINGS
@@ -13,6 +103,21 @@ write.table(in_SAODI2,file=paste("SAOD_index_1981_2007",out_prefix,".txt",sep=""
 s_SAODI <- subset(SAODI, year>1981 & year<2007) #Subset rows that correspond to the conditions
 s_SAODI<- subset(s_SAODI, select=-year) #Remove the column year
 in_SAODI<-as.vector(t(s_SAODI))   #This transform the data frame into a one colum
+write.table(in_SAODI,file=paste("SAOD_index_1981_2006",out_prefix,".txt",sep=""),sep=",")
+
+#Import results from MEOT and MSSA analyses with teleconneciton indices
+dat<-read.xls(infile3, sheet=1)
+tail(dat$Date_label)
+dat$SAOD<-in_SAODI  #Adding the SAOD index to all the MEOT/MSSA results in the data frame
+
+#Creating time series objects
+d_ts<-ts(data=dat,start=c(1982,1), end=c(2006,12), frequency=12, names=names(dat))
+colnames(d_ts)
+d_z<-as.zoo(d_ts)  #### THIS IS THE TIME SERIES OBJECT USED LATER ON
+
+##############################################################
+#### STEP 1: load image time series data, apply latitude weight for area
+
 write.table(in_SAODI,file=paste("SAOD_index_1981_2006",out_prefix,".txt",sep=""),sep=",")
 
 #Import results from MEOT and MSSA analyses with teleconneciton indices
@@ -64,7 +169,8 @@ SST_sgdf<-as(SST_rast,"SpatialGridDataFrame")
 SST_df<-as.data.frame(SST_sgdf)
 rm(SST_sgdf)
 
-
+#nv<-312
+SST_xy<-SST_df[,c("s1","s2")]
 
 ### DO PCA WITH SST_df: T-mode cross product from a standardized dataset...(i.e. correlation matrix)
 
@@ -72,9 +178,12 @@ A<-SST_df[1:(ncol(SST_df)-2)] #n*c or 42098*312
 At<-scale(A) #center and reduce using sd and mean
 sd(At[,2]) #this must be equal to 1 since At is standardized by columns
 mean(At[,2]) #this must be equal to 0 since At is standardized by columns
-
+names_var<-paste("t",1:ncol(At),sep="_")
+colnames(At)<-names_var
 cAt<-t(At)%*%At #T mode!! --> (c*n)by(n*c) should get c*c matrix or 312*312 
-diag(cAt) #equal 1 since it is a correlation
+diag(cAt) #equal 1 since it is a correlation, only if divided by n-1!!!
+cAt<-cAt/(nrow(At)-1)
+diag(cAt)
 #cAt<-cAt/nrow(At) #reduce by number of row...!!! THIS IS NOT NECESSARY SINCE At has been already based on standardized??
 #note that cAt is standardized and has been divided by n, so it is equivalent to a correlation matrix
 Et<-eigen(cAt)
@@ -84,100 +193,168 @@ Et<-eigen(cAt)
 pca_SST_t_mode<-principal(r=cAt, nfactors = npc, residuals = FALSE, covar=TRUE,rotate = "none")
 unclass(pca_SST_t_mode$loadings) # extract the matrix of ??
 
-sum(Et$value)
+(nrow(At)-1)*ncol(At)  #Sum of the diagonal is 42097*312
+sum(Et$value)   
 sum(diag(cAt))
 sum(pca_SST_t_mode$value)
+mean(Et$vectors)  #This is equal to zero since there was centering!!
+1/var(Et$vectors)[1]
+var(Et$vectors)[1]
+var(Et$vectors)[2]
+var(Et$vectors)[3]
+
+###COMPARING EIGENVECTORS FROM "eigen" and principal function
 
 head(unclass(pca_SST_t_mode$loadings)[,1:npc])
 head(Et$vectors[,1:npc]) #not equal...because not standardized...
-Estd <-  Et$vectors%*% diag(sqrt(Et$values))
-head(Estd)
-head(unclass(pca_SST$loadings)[,1:npc])
+cor(unclass(pca_SST_t_mode$loadings)[,1:npc],Et$vectors[,1:npc])
+diag(cor(unclass(pca_SST_t_mode$loadings)[,1:npc],Et$vectors[,1:npc]))
+plot(unclass(pca_SST_t_mode$loadings)[,1],Et$vectors[,1])
+unclass(pca_SST_t_mode$loadings)[,1]/Et$vectors[,1]
+##NOTE THAT THE RATIO IS 6.989 which is the square root of eigenvalue 1!!!
+(unclass(pca_SST_t_mode$loadings)[,1]/Et$vectors[,1])/sqrt(Et$value[1])
 
-##### Note that the scores are equal not equal but have correlation 1 and the ratio is the square root of eigenvalues...
-PC_scores<-At%*%Et$vectors
+Estd <-  Et$vectors%*% diag(sqrt(Et$values))  # Assign length to each eigenvectors??
+head(Estd[,1])
+head(unclass(pca_SST_t_mode$loadings)[,1])
+
+###COMPARIING SCORES FROM FROM "eigen" and principal function
+## Note that the scores are equal only if the square root of eigenvalues has been used to reduce (normalize) the Eigen Vectors!!!
+
+PC_scores<-At%*%Et$vectors # to
 pca_scores<-predict(pca_SST_t_mode,A)
 
 cor(PC_scores[,1],pca_scores[,1]) 
 plot(PC_scores[,1],pca_scores[,1]) 
 PC_scores[1:10,1]/pca_scores[1:10,1]
 sqrt(Et$values[1])
+(PC_scores[1:10,1]/sqrt(Et$values[1]))/pca_scores[1:10,1]
+plot(PC_scores[,1]/sqrt(Et$values[1]),pca_scores[,1]) 
 
-## DO PCA WITH SST_df: S-mode cross product
+mean(PC_scores[,1])
+sd(PC_scores[,1])
 
-A<-SST_df[1:(ncol(SST_df)-2)] #remove x,y fields to obtain a data.frame with 42098*312 dimension
-At<-t(A)
-At<-scale(At)
-A<-t(At)
-cAs<-At%*%A
-cAs<-cAs/nrow(cAs)
-Es<-eigen(cAs) #cross product S-mode
-diag(cAs)
-##Cross product PCA T mode
-pca_SST<-principal(r=cAs, nfactors = npc, residuals = FALSE, covar=TRUE,rotate = "none")
+##SO PCA scores from principal are standardized...!!! i.e. they have been divided by the sqrt of the eigenvalues???
 
-sum(Es$value)
-sum(diag(cAs))
-sum(pca_SST$value)
+#Now reduced version
+PC_std_scores<-At%*%Estd
+plot(PC_std_scores[,1],pca_scores[,1])
+PC_std_scores[1:10,1]/pca_scores[1:10,1]
 
+(PC_std_scores[1:10,1]/(Et$values[1]))/pca_scores[1:10,1]
 
+#
+pca_IDRISI_covar<-read.xls(infile_pca, sheet=1)
+pca_IDRISI_cor<-read.xls(infile_pca, sheet=2)
+pca_IDRISI_eigenvalues<-read.xls(infile_pca, sheet=3)
+pca_IDRISI_eigenvectors<-read.xls(infile_pca, sheet=4)
+pca_IDRISI_loadings<-read.xls(infile_pca, sheet=5)
 
-#cAt<-A%*%At
-#Ae<-eigen(cAt)
-#principal(r, nfactors = 1, residuals = FALSE,rotate="varimax",n.obs=NA, covar=FALSE, scores=FALSE,missing=FALSE,
-#          impute="median",oblique.scores=TRUE,method="regression",...)
+pca_scores_spdf<-cbind(as.data.frame(pca_scores),SST_xy)
 
+coordinates(pca_scores_spdf)<-pca_scores_spdf[,c("s1","s2")] #promote to spdf
+  
+pca_scores_lf<-pca_to_raster_fun(pca_scores_spdf,SST1_m,-9999,out_prefix)
 
+pca_SST_s<-stack(pca_scores_lf)
+NAvalue(pca_SST_s)<- -999
+plot(subset(pca_IDRISI_s,1))
 
-##Correlation PCA T mode
-#pca_SST<-principal(r=cAt, nfactors = npc, residuals = FALSE, covar=TRUE,rotate = "none")
+pca_IDRISI_s<-stack(mixedsort(list.files(pattern="pca_IDRISI_03302013_T-Mode_Cmp.*.RST$")))
+pca_IDRISI_s<-mask(pca_IDRISI_s,mask_land_NA)
+k=3
+plot(subset(pca_IDRISI_s,k))
+plot(subset(pca_SST_s,k))
+plot(stack(subset(pca_SST_s,k),subset(pca_IDRISI_s,k)))
+plot(subset(pca_SST_s,k),subset(pca_IDRISI_s,k))
 
-pca_SST_cor_t_mode<-principal(r=corAt, nfactors = npc, residuals = FALSE, covar=TRUE,rotate = "none")
-unclass(pca_SST_t_mode$loadings) # extract the matrix of ??
+## EXAMINE CORRELATION BETWEEN PCA FROM R AND FROM IDRISI...
 
-sum(Et$value)
-sum(diag(cAt))
-sum(pca_SST_t_mode$value)
-
-head(unclass(pca_SST_t_mode$loadings)[,1:npc])
-head(Et$vectors[,1:npc]) #not equal...because not standardized...
-Estd <-  Et$vectors%*% diag(sqrt(Et$values))
-head(Estd)
-head(unclass(pca_SST$loadings)[,1:npc])
-
-#cAt<-A%*%At
-#Ae<-eigen(cAt)
-#principal(r, nfactors = 1, residuals = FALSE,rotate="varimax",n.obs=NA, covar=FALSE, scores=FALSE,missing=FALSE,
-#          impute="median",oblique.scores=TRUE,method="regression",...)
+mat1_df<-as.data.frame(as(pca_SST_s,"SpatialGridDataFrame"))
+mat2_df<-as.data.frame(as(pca_IDRISI_s,"SpatialGridDataFrame"))
+cormat<-cor(mat1_df,mat2_df)
+diag(cormat)
 
 
-##############
-### 
-require(psych) 
-data(mtcars) 
-rawd <- mtcars[,c("am","carb","cyl","disp","drat","gear","hp","mpg")] #subset the original data.frame
+###################################################################
+###################################################################
 
-## compute acp 
-.PC <- princomp(~am+carb+cyl+disp+drat+gear+hp+mpg, cor=TRUE, data=mtcars) 
-pca <- principal(rawd, nfactors = 8, residuals = T, rotate="none", scores=T) 
-
-## eigenvectors of the correlation matrix of raw data 
-eigens <- eigen(cor(rawd)) #eigen value decomposition
-eigens$vectors #matrix of the eigenvectors, eigens is a list
-unclass(loadings(.PC))  # component 'loadings' in princomp parlance are the eigenvectors!!!
-
-## correlation matrix between raw data and unrotated component scores 
-## 'loadings' in ?principal parlance and 'component matrix' in SPSS --> loadings in princomp are the eigenvectors 
-# components scores can be obtained by multiplying the original data by the eigenvectors...
-eigens$vectors %*% diag(sqrt(eigens$values)) ##standardizing the eigenvectors...(it is already centered)
-cor(cbind(rawd, .PC$scores))  #loadings are the correlation between the PC scores and the origina variables
-unclass(pca$loadings) # extract the matrix of ??
-
-## extract un-rotated scores of principal components 
-head(scale(rawd) %*% eigens$vectors) # app, but very similar results 
-head(.PC$scores) 
-head(pca$scores) # scale'd, and obtained via regression on scale'd data 
-head(factor.scores(scale(rawd), 
-                   unclass(pca$loadings))) # same scores as from ?principal 
-#for differeneces between ?princomp and ?principal scores 
-#see last paragraph of Details in ?principal 
+# ## DO PCA WITH SST_df: S-mode cross product
+# PC_scores[1:10,1]/pca_scores[1:10,1]
+# A<-SST_df[1:(ncol(SST_df)-2)] #remove x,y fields to obtain a data.frame with 42098*312 dimension
+# At<-t(A)
+# At<-scale(At)
+# A<-t(At)
+# cAs<-At%*%A
+# cAs<-cAs/nrow(cAs)
+# Es<-eigen(cAs) #cross product S-mode
+# diag(cAs)
+# ##Cross product PCA T mode
+# pca_SST<-principal(r=cAs, nfactors = npc, residuals = FALSE, covar=TRUE,rotate = "none")
+# 
+# sum(Es$value)
+# sum(diag(cAs))
+# sum(pca_SST$value)
+# 
+# 
+# 
+# #cAt<-A%*%At
+# #Ae<-eigen(cAt)
+# #principal(r, nfactors = 1, residuals = FALSE,rotate="varimax",n.obs=NA, covar=FALSE, scores=FALSE,missing=FALSE,
+# #          impute="median",oblique.scores=TRUE,method="regression",...)
+# 
+# 
+# 
+# ##Correlation PCA T mode
+# #pca_SST<-principal(r=cAt, nfactors = npc, residuals = FALSE, covar=TRUE,rotate = "none")
+# 
+# pca_SST_cor_t_mode<-principal(r=corAt, nfactors = npc, residuals = FALSE, covar=TRUE,rotate = "none")
+# unclass(pca_SST_t_mode$loadings) # extract the matrix of ??
+# 
+# sum(Et$value)
+# sum(diag(cAt))
+# sum(pca_SST_t_mode$value)
+# 
+# head(unclass(pca_SST_t_mode$loadings)[,1:npc])
+# head(Et$vectors[,1:npc]) #not equal...because not standardized...
+# Estd <-  Et$vectors%*% diag(sqrt(Et$values))
+# head(Estd)
+# head(unclass(pca_SST$loadings)[,1:npc])
+# 
+# #cAt<-A%*%At
+# #Ae<-eigen(cAt)
+# #principal(r, nfactors = 1, residuals = FALSE,rotate="varimax",n.obs=NA, covar=FALSE, scores=FALSE,missing=FALSE,
+# #          impute="median",oblique.scores=TRUE,method="regression",...)
+# 
+# 
+# ##############
+# ### 
+# require(psych) 
+# data(mtcars) 
+# rawd <- mtcars[,c("am","carb","cyl","disp","drat","gear","hp","mpg")] #subset the original data.frame
+# 
+# ## compute acp 
+# .PC <- princomp(~am+carb+cyl+disp+drat+gear+hp+mpg, cor=TRUE, data=mtcars) 
+# pca <- principal(rawd, nfactors = 8, residuals = T, rotate="none", scores=T) 
+# 
+# ## eigenvectors of the correlation matrix of raw data 
+# eigens <- eigen(cor(rawd)) #eigen value decomposition
+# eigens$vectors #matrix of the eigenvectors, eigens is a list
+# unclass(loadings(.PC))  # component 'loadings' in princomp parlance are the eigenvectors!!!
+# 
+# ## correlation matrix between raw data and unrotated component scores 
+# ## 'loadings' in ?principal parlance and 'component matrix' in SPSS --> loadings in princomp are the eigenvectors 
+# # components scores can be obtained by multiplying the original data by the eigenvectors...
+# eigens$vectors %*% diag(sqrt(eigens$values)) ##standardizing the eigenvectors...(it is already centered)
+# cor(cbind(rawd, .PC$scores))  #loadings are the correlation between the PC scores and the origina variables
+# unclass(pca$loadings) # extract the matrix of ??
+# 
+# ## extract un-rotated scores of principal components 
+# head(scale(rawd) %*% eigens$vectors) # app, but very similar results 
+# head(.PC$scores) 
+# head(pca$scores) # scale'd, and obtained via regression on scale'd data 
+# head(factor.scores(scale(rawd), 
+#                    unclass(pca$loadings))) # same scores as from ?principal 
+# #for differeneces between ?princomp and ?principal scores 
+# #see last paragraph of Details in ?principal 
+# rm(PC_std_scores)
