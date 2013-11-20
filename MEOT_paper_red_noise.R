4ac80d6eb33c3aa46f302f7def676e2c6dce87ed
Benoit Parmentier <benoit.parmentier@gmail.com>
2013-11-20 10:45:00 -0500

    rednoise analyses adding prediogram for revisions analyses

--- MEOT_paper_red_noise.R
+++ MEOT_paper_red_noise.R
@@ -1,15 +1,17 @@
 ####################################    MEOT-MSSA PAPER   #######################################
 ############################      SCRIPT 3- MEOT          #######################################
-#This script reads SAODI index and other telectonnection indices...                             #
-#It examines indices against background red noise.                                                   #
-#Note that spatial patterns from MEOT and MSSA components are not analyzed in this script       #                 #
-#AUTHOR: Benoit Parmentier                                                                      #
-#DATE: 11/02/2013           
+#This script reads SAODI index and other telectonnection indices...                            
+#It examines indices against background red noise.                                                  
+#Note that spatial patterns from MEOT and MSSA components are not analyzed in this script                      
+#AUTHOR: Benoit Parmentier                                                                      
+#DATE: 11/07/2013           
 #Version: 4
-#PROJECT: Clark Labs Climate predction- MEOT/MSSA paper                                         #
+#PROJECT: Clark Labs Climate predction- MEOT/MSSA paper                                         
+#
 #################################################################################################
 
 ###Loading R library and packages                                                      
+#
 library(gtools)                              # loading some useful tools 
 library(mgcv)                                # GAM package by Simon Wood
 library(sp)                                  # Spatial pacakge with class definition by Bivand et al.
@@ -26,6 +28,7 @@ library(forecast)                            # package containing ARIMA procedur
 library(rasterVis)
 library(plotrix)
 library(matrixStats) 
+#
 ### Parameters and argument
 
 infile1<-"SAODI-01-1854_06-2011_test.asc"             #GHCN shapefile containing variables for modeling 2010                 
@@ -54,7 +57,7 @@ mode_list_MEOT<-c("MEOT1","MEOT3", "MEOT4","MEOT7","MEOT10","MEOT15","MEOT16")
 mode_list_PCA<-c("MSSA1","MSSA2","MSSA3","MSSA4","MSSA5","MSSA6")    
 #mode_list_PCA<-paste("MSSA",1:15,sep="")
 
-lag_window<-13
+lag_window<- 13
 
 ############### START OF SCRIPT  #####################
 
@@ -118,23 +121,17 @@ absext_lag<-tmp$lag[pos,1,1] #This is the lag corresponding to the maximum absol
 # correlation amongh all telectonnections...
 
 ########## CREATING RED  NOISE ###########
-no_sim<- 100
-no_t<-nrow(dat)
-ar1_coeff<-0.5
 
-test_AR1 <- arima.sim(n=no_t, list(ar=c(ar1_coeff)))
-stest<-spectrum(test_AR1)
-
-ar1_sim_fun<-function(i,no_t,ar1_coeff,ref_ts){
+ar1_sim_fun<-function(i,no_t,ar1_coeff,ref_ts,sd_val=NULL){
+  #no_t: length of time series
+  #ar1_coeff: coef fitted from AR model
   
-  #test_AR1 <- arima.sim(n=no_t, list(ar=c(ar1_coef)))
-  #d_ts<-ts(data=test_AR1,start=c(1982,1), end=c(2006,12), frequency=12, names="AR1_test")
-  #colnames(d_ts)
-  #d_AR1<-as.zoo(d_ts)  #### THIS IS THE TIME SERIES OBJECT USED LATER ON
+  if(is.null(sd_val)){
+    ts_AR1 <- arima.sim(n=no_t, list(ar=c(ar1_coeff)))
+  }else{
+    ts_AR1 <- arima.sim(n=no_t, list(ar=c(ar1_coeff)),sd=sd_val)
+  }
   
-  #d_AR1_spec<-spectrum(d_AR1,plot=FALSE)
-  
-  ts_AR1 <- arima.sim(n=no_t, list(ar=c(ar1_coeff)))
   s1<-spectrum(ts_AR1,plot=FALSE)
   
   power_spec <- s1$spec
@@ -142,128 +139,49 @@ ar1_sim_fun<-function(i,no_t,ar1_coeff,ref_ts){
   return(as.data.frame(power_spec))
 }
 
-l_ar1<-lapply(1:no_sim,FUN=ar1_sim_fun,no_t=no_t,ar1_coeff=ar1_coeff)
+sine_structure_fun <-function(x,T,phase,a,b){
+  #Create sine for a one dimensional series
+  #Note that sine function uses radian unit.
+  #a=amplitude
+  #b=mean or amplitude 0 of the series
+  #T= stands for period definition
+  #phase=phase angle (in radian!!)
+#  y <- a*sin((x*pi/T)+ phase) + b
+  
+  y <- a*sin((x*2*pi/T)+ phase) + b
+}
 
-lf<-do.call(cbind,l_ar1)
-lf$freq <-stest$freq
-mean_power<-rowMeans(lf[,1:100])
-sd_power<-rowSds(lf[,1:100])
+sd.interval <- function(data, conf.level = 0.95) {
+  df = length(data) - 1
+  chilower = qchisq((1 - conf.level)/2, df)
+  chiupper = qchisq((1 - conf.level)/2, df, lower.tail = FALSE)
+  v = sd(data)
+  ci_range <- as.data.frame(cbind(df * v/chiupper, df * v/chilower)) 
+}
 
-plot(lf$freq,mean_power,type="b")
-
-y <- mean_power
-x<- lf$freq
-no <- no_sim
-y_sd <- sd_power
-ciw   <- qt(0.975, no,) * y_sd / sqrt(no)
-plotCI(y=y, x=x, uiw=ciw, col="red", main=paste(" Spectrum for REd noise"), barcol="blue", lwd=1,
-       ylab="mean power (A^2)", xlab="frequency")
-lines(y~x)
-#ciw   <- qt(0.975, no) * y_sd / sqrt(no)
-
-#if(i==1){
-#  plotCI(y=y, x=x, uiw=ciw, col=col_t[i], main=paste(" Comparison of ",metric_name," in ",mod_name,sep=""), barcol="blue", lwd=1,
-#         ylab="RMSE (C)", xlab=xlab_text)
-#  lines(y~x, col=col_t[i])
-
-#}else{
-#  lines(y~x, col=col_t[i])
-#}
-
-hist(as.numeric(lf[1,])) #note that the spectrum is chi-sqqured because it is a variance!!!!!
-df<-no_t-1
-dchisq(as.numeric(lf[1,1]), df, ncp=0, log = FALSE)
-
-
-coverage<-0.95
-tail <- (1 - coverage)
-#df <- spec.obj$df
-df<-no_t-1
-upper.quantile <- 1 - tail * pchisq(df, df, lower.tail = FALSE)
-lower.quantile <- tail * pchisq(df, df)
-1/(qchisq(c(upper.quantile, lower.quantile), df)/df)
-
-#Get the CI for each spectrum??
-
-
-#calculate the diff between ref time series in spectrum and the actual time
-#series...
-
-#install.packages("portes")
-#BoxPierce
-#test power spectrum  in R
-arima_mod <- arima(d_z$MEOT1, order = c(1,0,0))
-arima_mod <- arima(d_z$MEI, order = c(1,0,0))
-arima_mod <- acf(d_z$MEI)
-arima_mod <- acf(d_z$MEOT1)
-arima_mod <- pacf(d_z$MEOT1)
-arima_mod <- pacf(d_z$MEI)
-
-arima_mod <- arima(d_z$MEOT1, order = c(1,0,0))
-no_sim<- 100
-no_t<-nrow(dat)
-ar1_coef<-arima_mod$coef[1]
-frequency(d_z$MEOT1) #12
-
-test_AR1 <- arima.sim(n=no_t, list(ar=c(ar1_coef)))
-stest<-spectrum(test_AR1)
-plot(stest$spec~(1/stest$freq),type="b")
-
-l_ar1<-lapply(1:no_sim,FUN=ar1_sim_fun,no_t=no_t,ar1_coeff=ar1_coef)
-lf<-do.call(cbind,l_ar1)
-lf$freq <-stest$freq
-mean_power<-rowMeans(lf[,1:100])
-sd_power<-rowSds(lf[,1:100])
-
-plot(1/lf$freq,mean_power,type="b")
-plot(lf$freq,mean_power,type="b")
-
-y <- mean_power
-x<- lf$freq
-no <- no_sim
-y_sd <- sd_power
-ciw   <- qt(0.975, no,) * y_sd / sqrt(no)
-plotCI(y=y, x=x, uiw=ciw, col="red", main=paste(" Spectrum for REd noise"), barcol="blue", lwd=1,
-       ylab="mean power (A^2)", xlab="frequency")
-lines(y~x)
-#ciw   <- qt(0.975, no) * y_sd / sqrt(no)
-
-#if(i==1){
-#  plotCI(y=y, x=x, uiw=ciw, col=col_t[i], main=paste(" Comparison of ",metric_name," in ",mod_name,sep=""), barcol="blue", lwd=1,
-#         ylab="RMSE (C)", xlab=xlab_text)
-#  lines(y~x, col=col_t[i])
-
-#}else{
-#  lines(y~x, col=col_t[i])
-#}
-
-hist(as.numeric(lf[1,])) #note that the spectrum is chi-sqqured because it is a variance!!!!!
-df<-no_t-1
-dchisq(as.numeric(lf[1,1]), df, ncp=0, log = FALSE)
-
-test_AR1 <- arima.sim(n=no_t, list(ar=c(ar1_coef)))
-d_ts<-ts(data=test_AR1,start=c(1982,1), end=c(2006,12), frequency=12, names="AR1_test")
-colnames(d_ts)
-d_AR1<-as.zoo(d_ts)  #### THIS IS THE TIME SERIES OBJECT USED LATER ON
-
-d_AR1_spec<-spectrum(d_AR1,plot=TRUE)
-stest<-spectrum(d_z$MEOT1,plot=FALSE)
-stest<-spectrum(d_z$MEOT1,plot=TRUE)
-stest$freq
-lines(stest$spec~stest$freq)
-plot(stest$spec~stest$freq,type="h")
-
-plot(d_AR1_spec$spec ~ d_AR1_spec$freq,type="b",col="red")
-lines(stest$spec~stest$freq,type="b")
-
-plot(d_AR1_spec$spec ~ 1/d_AR1_spec$freq,type="b",col="red")
-lines(stest$spec~1/stest$freq,type="b")
+#http://svn.r-project.org/R/trunk/src/library/stats/R/spectrum.R
+spec.ci <- function (spec.obj, coverage = 0.95)
+{
+  ## A utility function for plot.spec which calculates the confidence
+  ## interval (centred around zero). We use a conditional argument to
+  ## ensure that the ci always contains zero.
+  
+  if (coverage < 0 || coverage >= 1)
+    stop("coverage probability out of range [0,1)")
+  tail <- (1 - coverage)
+  df <- spec.obj$df
+  upper.quantile <- 1 - tail * pchisq(df, df, lower.tail = FALSE)
+  lower.quantile <- tail * pchisq(df, df)
+  1/(qchisq(c(upper.quantile, lower.quantile), df)/df)
+}
 
 ######## PLOTTING ON THE SAME SCALE TEH SPECTRUM
 
 #Fit a model for MEOT1 index
 arima_mod <- arima(d_z$MEOT1, order = c(1,0,0))
-arima_mod <- arima(as.vector(d_z$MEOT1), order = c(1,0,0))
+arima_mod <- arima(as.numeric(d_z$MEOT1), order = c(1,0,0))
+arima_mod_AMM<- arima(as.vector(d_z$AMM), order = c(1,0,0))
+
 no_sim<- 10000
 no_t<-nrow(dat)
 ar1_coef<-arima_mod$coef[1]
@@ -277,13 +195,14 @@ stelind<-spectrum(as.vector(d_z$MEOT1),plot=FALSE)
 #stest<-spectrum(d_z$MEOT1,plot=TRUE)
 plot(d_AR1_spec$spec ~ d_AR1_spec$freq,type="b",col="red")
 lines(stelind$spec~stelind$freq,type="b")
-
+set.seed(100)
 l_ar1<-lapply(1:no_sim,FUN=ar1_sim_fun,no_t=no_t,ar1_coeff=ar1_coef)
 lf<-do.call(cbind,l_ar1)
-lf$freq <-stest$freq
+lf$freq <-d_AR1_spec$freq
 mean_power<-rowMeans(lf[,1:no_sim])
 sd_power<-rowSds(lf[,1:no_sim])
-
+median_power <- median(m_dat[,1])
+hist(log(lf[,1:1]))
 plot(mean_power ~ lf$freq,type="b",col="red")
 lines(stelind$spec~stelind$freq,type="b")
 
@@ -304,9 +223,100 @@ plotCI(y=y, x=x, uiw=ci_bands[,1],liw=ci_bands[,2], col="red", main=paste(" Spec
        ylab="mean power (A^2)", xlab="frequency")
 lines(y~x)
 lines(stelind$spec~stelind$freq,type="b")
+
+tt<-t.test(m_dat[,1]-y[1])
+
+##test based on slope??
+plot(1:150,as.vector(log(m_dat[1,])))
+points(1:150,as.vector(log(m_dat[2,])),col="red")
+##test based on slope??
+plot(log(1:150),as.vector(log(m_dat[1,])))
+points(log(1:150),as.vector(log(m_dat[2,])),col="red")
+##test based on slope??
+plot(log(1:150,base=10),as.vector(log(m_dat[1,])))
+points(log(1:150,base=10),as.vector(log(m_dat[2,])),col="red")
+
+y_f<- as.numeric(log(m_dat[1,]))
+x_f<-log(1:150,base=10)
+x_f<-lf$freq
+d_f <- data.frame(x_f,y_f)
+lm_m1<-lm(y_f~x_f,data=d_f)
+summary(lm_m1)
+
+get_coef<-function(y_f,x_f){
+  d_f <- data.frame(as.numeric(log(x_f)),log(as.numeric(y_f)))
+  lm_m1<-lm(y_f~x_f,data=d_f)
+  #summary(lm_m1)
+  coef(lm_m1)[2]
+}
+
+m_dat2 <- as.data.frame(t(as.matrix(m_dat)))
+tt<-as.numeric(lapply(m_dat2,FUN=get_coef,x_f=1:150))
+
+hist(as.numeric(tt))
+val_telind<-get_coef(as.numeric(stelind$spec),stelind$freq)
+
+m <- mean(tt)
+s <- sd(tt)
+n <- length(tt)
+error <- qt(0.975,df=n-1)*s/sqrt(n)
+left <- m-error
+right <- m+error
+ci<-cbind(left,right)
+ci
+val_telind-m/s
+
+#pnorm(val_telind-m/s)
+
+### Ok CI should be based on the log transform!!!
+
+m_datlog<-log(m_dat) #log transform all columns...
+mean_power_log<-colMeans(m_datlog)
+sd_power_log <-colSds(m_datlog)
+hist(m_datlog[,1])
+plot(mean_power_log ~ loglf$freq),type="b",col="red")
+
+stelind<-spectrum(as.vector(d_z$MEOT1),plot=FALSE)
+lines(log(stelind$spec)~(stelind$freq),type="b")
+stelind2<-spectrum(as.numeric(d_z$MEOT1)-mean(as.numeric(d_z$MEOT1)),plot=FALSE)
+lines(log(stelind2$spec)~log(stelind2$freq),type="b",col="green")
+
+y <- mean_power_log
+x<- log(lf$freq)
+x<- (lf$freq)
+no <- no_sim
+
+ciw   <- qt(0.975, no,) * y_sd / sqrt(no)
+plotCI(y=y, x=x, uiw=ciw,liw=ciw, col="red", main=paste(" Spectrum for REd noise"), barcol="blue", lwd=1,
+       ylab="log mean power (A^2)", xlab="log frequency",ylim=c(-8,8))
+lines(y~x)
+lines(log(stelind$spec)~log(stelind$freq),type="b",col="black")
+lines(log(stelind$spec)~(stelind$freq),type="b")
+lines(log(stelind2$spec)~(stelind2$freq),type="b",col="blue")
+#lines(log(stelind$spec)~log(stelind$freq),type="b",col="black")
+#plot(log(stelind2$spec)~(stelind2$freq),type="b")
+lines(log(stelind2$spec)~(stelind2$freq),type="b")
+
+m_datlog<-log(m_dat)
+l_logCI<- lapply(m_datlog,FUN=sd.interval,conf.level = 0.95)
+
+ci_log_bands<-do.call(rbind,l_logCI)
+
+
+#ciw   <- qt(0.975, no,) * y_sd / sqrt(no)
+plotCI(y=y, x=x, uiw=ci_log_bands[,1],liw=ci_log_bands[,2], col="red", main=paste(" Spectrum for REd noise"), barcol="blue", lwd=1,
+       ylab="mean power (A^2)", xlab="frequency")
+lines(y~x)
+lines(log(stelind$spec)~stelind$freq,type="b")
+
+## DO CORRELATION MATRIX FOR ALL INDICES AS WELL AS SPECTRUM TO FIND DOMINANT FREQ
+
 stest<-spectrum(as.vector(d_z$MEI))
 telind<-c("PNA","NAO","TNA","TSA","SAOD","MEI","PDO","AO","AAO","AMM","AMOsm","QBO")
 mode_list_MEOT<-c("MEOT1","MEOT3", "MEOT4","MEOT7","MEOT10","MEOT15","MEOT16")
+
+mat_telind <- as.matrix(subset(d_z,select=telind))
+cor_x<-round(cor(mat_telind),digit=2)
 
 coef_list<-vector("list",length(telind))
 for (i in 1:length(telind)){
@@ -318,60 +328,32 @@ for (i in 1:length(telind)){
 }
 
 unlist(coef_list)
+print(cbind(telind,round(as.numeric(coef_list),digit=3)))
 
-## DO CORRELATION MATRIX FOR ALL INDICES AS WELL AS SPECTRUM TO FIND DOMINANT FREQ
-
+d_z_telind <- subset(d_z,stelind) #matix of of correlation between indices
 
 tmp<-ccf(d_z$SAOD,d_z$AMM, lag=13)  #Note that ccf does not take
 lag_m<- -13:13
 tmp$lag[,1,1]<-lag_m  #replacign lag values because continuous
 
-ys1<-sine_structure_fun(1:300,12,0,2,0)
-ys2<-sine_structure_fun(1:300,6,0,2,0)
-ys3<-sine_structure_fun(1:300,150,0,2,0)
+x<- 1:300
+ys1<-sine_structure_fun(x,12,0,2,0) 
+ys2<-sine_structure_fun(x,24,0,2,0)
+ys3<-sine_structure_fun(x,300,0,2,0) #fundamental harmonic
+ys4<- 0.04*x 
+ys5<-rnorm(300,sd=0.2)
+y_test <- ys1 + ys2 + ys3 + ys4 + ys5
 
-y_test <- ys1 + ys2 + ys3
-cpgram(y_test)
+#cpgram(y_test)
 plot(y_test,type="b")
 y_test_spec <-spectrum(y_test)
 y_test_spec <-spectrum(y_test,log=c("no")) #no log 
-
-periodgram(y_test)
+plot(ts(cbind(ys1,ys2,ys3,ys4,y_test)))
+#periodgram(y_test)
 plot(y_test_spec$spec~1/y_test_spec$freq,type="h")
 f1<- ((y_test_spec$freq*150*4)/300)
 plot(y_test_spec$spec~ f1,type="h")
-sine_structure_fun <-function(x,T,phase,a,b){
-  #Create sine for a one dimensional series
-  #Note that sine function uses radian unit.
-  #a=amplitude
-  #b=mean or amplitude 0 of the series
-  #T= stands for period definition
-  #phase=phase angle (in radian!!)
-  
-  y <- a*sin((x*pi/T)+ phase) + b
-}
 
-sd.interval <- function(data, conf.level = 0.95) {
-       df = length(data) - 1
-       chilower = qchisq((1 - conf.level)/2, df)
-       chiupper = qchisq((1 - conf.level)/2, df, lower.tail = FALSE)
-       v = sd(data)
-       ci_range <- as.data.frame(cbind(df * v/chiupper, df * v/chilower)) 
-}
+## Simulate red noise:
 
-#http://svn.r-project.org/R/trunk/src/library/stats/R/spectrum.R
-spec.ci <- function (spec.obj, coverage = 0.95)
-{
-  ## A utility function for plot.spec which calculates the confidence
-  ## interval (centred around zero). We use a conditional argument to
-  ## ensure that the ci always contains zero.
-  
-  if (coverage < 0 || coverage >= 1)
-    stop("coverage probability out of range [0,1)")
-  tail <- (1 - coverage)
-  df <- spec.obj$df
-  upper.quantile <- 1 - tail * pchisq(df, df, lower.tail = FALSE)
-  lower.quantile <- tail * pchisq(df, df)
-  1/(qchisq(c(upper.quantile, lower.quantile), df)/df)
-}
 
