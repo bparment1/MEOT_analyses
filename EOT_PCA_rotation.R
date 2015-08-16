7cbde8d855b41d95ad178296d1936dfedd156390
Benoit Parmentier <benoit.parmentier@gmail.com>
2015-08-15 21:45:00 -0400

    adding spss results and comparison between pca

--- EOT_PCA_rotation.R
+++ EOT_PCA_rotation.R
@@ -5,7 +5,7 @@
 #
 #AUTHOR: Benoit Parmentier                                                                       #
 #DATE CREATED: 07/02/2015 
-#DATE MODIFIED: 07/02/2015
+#DATE MODIFIED: 08/15/2015
 #
 #PROJECT: Land transitions from Remote Sensing
 ##################################################################################################
@@ -23,6 +23,10 @@ library(plotrix)                            # plot options and functions includi
 library(rasterVis)                          # raster visualization
 library(colorRamps)                         # contains matlab.like palette
 library(lsr)
+library(psych)
+library(GPArotation)
+library(zoo)
+library(xts)
 
 #################################################
 ###### Functions  used in the script  ##########
@@ -52,19 +56,50 @@ cv_test_fun <- function(x,y) {
   chisq_val <- chisq.test(x, y, correct=FALSE)
   CV = sqrt(chisq_val$statistic /
               (length(x) * (min(length(unique(x)),length(unique(y))) - 1)))
-  print.noquote("Cramér V / Phi:")
+  print.noquote("Cram?r V / Phi:")
   CV <- as.numeric(CV)
   cv_obj<- list(CV=CV,chisq_val=chisq_val)
   return(cv_obj)
 }
 
+
+pca_to_raster_fun<-function(pc_spdf,ref_raster,NA_val,out_prefix){
+  #Input arguments:
+  #pc_spdf: spdf with scores components and
+  #         must include x,y in the last 2 columns
+  #ref_raster: reference raster giving the output extent and NA if present
+  #NA_val<- values assigned to NA pixels, if "NA", use default
+  
+  npc<-ncol(pc_spdf)-2 #number of components 
+  pc_scores_lf<-vector("list",npc) 
+  for (k in 1:npc){
+    pc_scores<-pc_spdf[,k] #vector containing scores for components k
+    pc_name<-names(pc_spdf)[k] #name of the current component
+    raster_name<-paste("pc_component_",k,"_",out_prefix,".rst",sep="")
+    
+    if (NA_val=="NA"){
+      pc_lag<-rasterize(pc_scores,ref_raster,pc_name,fun=min,overwrite=TRUE,
+                        filename=raster_name)
+    }
+    #Use provided NA value for the background
+    if (NA_val!="NA"){
+      pc_lag<-rasterize(pc_scores,ref_raster,pc_name,background=NA_val,fun=min,overwrite=TRUE,
+                        filename=raster_name)
+    }
+    pc_scores_lf[[k]]<-raster_name
+  } 
+  return(pc_scores_lf)
+}
 #############################################
 ######## Parameters and arguments  ########
 
-inDir <- "J:/Benoit/Data/MEOT_analyses_02202015"
+#inDir <- "J:/Benoit/Data/MEOT_analyses_02202015"
+inDir <- "/home/parmentier/Data/MEOT12272012/Papers_writing_MEOT/MEOT_analyses_02202015"
+
 SST_dir <- "SST_1982_2007"
 mask_fname <- "mask_rgf_1_1.tif"
 out_suffix <-"_eot_pca_07022015"
+CRS_WGS84 <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +towgs84=0,0,0" #Station coords WGS84 # CONST 2
 
 lf_sst <- list.files(path=file.path(inDir,SST_dir),pattern=".rst$",full.names=T)
 
@@ -75,15 +110,18 @@ infile1<-"SAODI-01-1854_06-2011_test.asc"             #GHCN shapefile containing
 infile2<-"SAODI-01-1854_06-2011.csv"                     #List of 10 dates for the regression
 infile3<-"MEOT_MSSA_Telcon_indices_12112012.xlsx"                        #LST dates name
 #infile4<-"mask_rgf_1_1.rst"
-infile_pca <-"pca_IDRISI_03302013_T-Mode_DIF.xlsx"
+infile_pca_IDRISI <-"pca_IDRISI_03302013_T-Mode_DIF.xlsx"
+
+infile_pca_spss <- "spss_pca_output_08082015.xlsx"
+
 infile_s_mode_pca  <- "pca_IDRISI_04152013_S-Mode_DIF.xlsx"
-npc<-20 # number of pca to produce...put this at the beginning of the script...
-lag_window<-13
+npc <- 10 # number of pca to produce...put this at the beginning of the script...
+lag_window <-13
 telind<-c("PNA","NAO","TNA","TSA","SAOD","MEI","PDO","AO","AAO","AMM","AMOsm","QBO")
 mode_list_MEOT<-c("MEOT1","MEOT3", "MEOT4","MEOT7","MEOT10","MEOT15","MEOT16")
 mode_list_PCA<-c("MSSA1","MSSA2","MSSA3","MSSA4","MSSA5","MSSA6")    
 #mode_list_PCA<-paste("MSSA",1:15,sep="")
-out_prefix<-"pca_test_04152013"
+#out_prefix<-"pca_test_04152013"
 
 setwd(inDir)
 
@@ -112,9 +150,11 @@ if(create_outDir_param==TRUE){
 #s_SAODI2<- subset(s_SAODI2, select=-year) #Remove the column year
 #in_SAODI2<-as.vector(t(s_SAODI2))   #This transform the data frame into a one colum
 #write.table(in_SAODI2,file=paste("SAOD_index_1981_2007",out_prefix,".txt",sep=""),sep=",")
-
+ 
 r_sst <- stack(lf_sst)
+projection(r_sst) <- CRS_WGS84 #assign projection
 r_mask <- raster(file.path(inDir,mask_fname))
+projection(r_mask) <- CRS_WGS84
 r_mask_NA <- r_mask
 NAvalue(r_mask_NA) <- 0
 #NAvalues()
@@ -137,6 +177,8 @@ writeRaster(r_sst_m,filename="ANOM_SST_1982_2007_weighted.tif",overwrite=TRUE)
 rm(r_sst)
 r_sst <-brick("ANOM_SST_1982_2007_weighted.tif")
 
+SST1_m <- subset(r_sst,1)
+  
 ##############################################################
 #### STEP 2: get data ready for PCA by lagging data frame...
 
@@ -174,12 +216,17 @@ Et<-eigen(cAt)
 
 ##Cross product PCA T mode
 #pca_SST<-principal(r=cAt, nfactors = npc, residuals = FALSE, covar=TRUE,rotate = "none")
-pca_SST_t_mode<-principal(r=cAt, nfactors = npc, residuals = FALSE, covar=TRUE,rotate = "none")
+pca_SST_t_mode<-principal(r=cAt, nfactors = npc, residuals = FALSE, covar=TRUE,rotate = "none") #psych package
 unclass(pca_SST_t_mode$loadings) # extract the matrix of ??
+head(unclass(pca_SST_t_mode$loadings)) # extract the matrix of ??
+#<- as.data.frame(unclass(pca_SST_t_mode$loadings))) # extract the matrix of ??
+
+pca_SST_t_mode_varimax <-principal(r, nfactors = npc, residuals = FALSE,rotate="varimax",n.obs=NA, covar=FALSE, scores=FALSE,missing=FALSE,
+          impute="median",oblique.scores=TRUE,method="regression",...)
 
 (nrow(At)-1)*ncol(At)  #Sum of the diagonal is 42097*312
-sum(Et$value)   
-sum(diag(cAt))
+sum(Et$value)   #equal to 312
+sum(diag(cAt))  #
 sum(pca_SST_t_mode$value)
 mean(Et$vectors)  #This is equal to zero since there was centering!!
 1/var(Et$vectors)[1]
@@ -187,21 +234,38 @@ var(Et$vectors)[1]
 var(Et$vectors)[2]
 var(Et$vectors)[3]
 
-#
-pca_IDRISI_covar<-read.xls(infile_pca, sheet=1)
-pca_IDRISI_cor<-read.xls(infile_pca, sheet=2)
-pca_IDRISI_eigenvalues<-read.xls(infile_pca, sheet=3)
-pca_IDRISI_eigenvectors<-read.xls(infile_pca, sheet=4)
-pca_IDRISI_loadings<-read.xls(infile_pca, sheet=5)
+## read in IDRISI and SPSS results
+pca_IDRISI_covar<-read.xls(file.path(inDir,infile_pca_IDRISI), sheet=1)
+pca_IDRISI_cor<-read.xls(file.path(inDir,infile_pca_IDRISI), sheet=2)
+pca_IDRISI_eigenvalues<-read.xls(file.path(inDir,infile_pca_IDRISI), sheet=3)
+pca_IDRISI_eigenvectors<-read.xls(file.path(inDir,infile_pca_IDRISI), sheet=4)
+pca_IDRISI_loadings<-read.xls(file.path(inDir,infile_pca_IDRISI), sheet=5)
 
+spss_data_loadings_pca_unrotated <-read.xls(file.path(inDir,infile_pca_spss), sheet="loadings_pca_unrotated") #this is T-mode using cor matrix
+spss_data_loadings_pca_varimax <-read.xls(file.path(inDir,infile_pca_spss), sheet="loadings_pca_rotated") #this is T-mode using cor matrix, varimax rotation
+
+ 
 ###### CREATE SCORES IMAGES USING PREDICTED SCORES FROM PRINCIPAL
-pca_scores<-predict(pca_SST_t_mode,A)  #generate scores from original matrix and object
+pca_scores <- predict(pca_SST_t_mode,A)  #generate scores from original matrix and object
 pca_scores_spdf<-cbind(as.data.frame(pca_scores),SST_xy) #add coordinates
 coordinates(pca_scores_spdf)<-pca_scores_spdf[,c("s1","s2")] #promote to spdf
 
 #Now generate raster images...
-pca_scores_lf<-pca_to_raster_fun(pca_scores_spdf,SST1_m,-9999,out_prefix)
+pca_scores_lf<-pca_to_raster_fun(pca_scores_spdf,SST1_m,-9999,out_suffix)
 
 pca_SST_s<-stack(pca_scores_lf)
 NAvalue(pca_SST_s)<- -9999
 #plot(subset(pca_IDRISI_s,1))
+
+
+
+### Comparison Loadings and scores between R, SPSS and IDRISI
+
+
+plot(spss_data_loadings_pca_unrotated$pc1,unclass(pca_SST_t_mode$loadings)[,1])
+cor(spss_data_loadings_pca_unrotated$pc1,unclass(pca_SST_t_mode$loadings)[,1])
+
+plot(pca_IDRISI_loadings$CMP.1,unclass(pca_SST_t_mode$loadings)[,1])
+cor(pca_IDRISI_loadings$CMP.1,unclass(pca_SST_t_mode$loadings)[,1])
+
+
